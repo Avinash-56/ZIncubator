eaaa419a7b033f8acc878852aac04ada50121a52
unknown <avinashupadhyay56@gmail.com>
2020-12-20 14:49:15 +0530

    Bugs persist

--- lib/Screens/IncubatorLogin/components/body.dart
+++ lib/Screens/IncubatorLogin/components/body.dart
@@ -6,54 +6,127 @@ import 'package:flutter_auth/components/rounded_button.dart';
 import 'package:flutter_auth/components/rounded_input_field.dart';
 import 'package:flutter_auth/components/rounded_password_field.dart';
 
-class Body extends StatelessWidget {
-  const Body({
-    Key key,
-  }) : super(key: key);
+class Body extends StatefulWidget {
+  @override
+  _BodyState createState() => _BodyState();
+}
+
+class _BodyState extends State<Body> {
+  final GlobalKey<FormState> _formKey = GlobalKey();
+  Map<String, String> _authData = {'email': '', 'password': ''};
+
+  void _showErrorDialoge(String msg) {
+    showDialog(
+        context: context,
+        builder: (ctx) => AlertDialog(
+              title: Text('An Error Occured'),
+              content: Text(msg),
+              actions: <Widget>[
+                FlatButton(
+                  child: Text('Okay'),
+                  onPressed: () {
+                    Navigator.of(ctx).pop();
+                  },
+                )
+              ],
+            ));
+  }
+
+  Future<void> submit() async {
+    if (!_formKey.currentState.validate()) {
+      return;
+    }
+    _formKey.currentState.save();
+    try {
+      await Provider.of<AuthenticationService>(context, listen: false).signIn(
+        _authData['email'],
+        _authData['password'],
+      );
+      Navigator.of(context).pushReplacementNamed(UserStartup.routNamee);
+    } catch (e) {
+      var errorMessage = 'Authentication Failed. Try Again';
+      _showErrorDialoge(errorMessage);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return Background(
-      child: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              "Please login to see and approve startups",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                color: Colors.red,
-                fontSize: 24,
-              ),
+
+    return Stack(
+      children: <Widget>[
+        Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Colors.lightGreenAccent,
+                Colors.blue,
+              ],
             ),
-            SizedBox(height: size.height * 0.03),
-            SizedBox(height: size.height * 0.03),
-            RoundedInputField(
-              hintText: "Your Email",
-              onChanged: (value) {},
-            ),
-            RoundedPasswordField(
-              onChanged: (value) {},
-            ),
-            RoundedButton(
-              text: "LOGIN",
-              press: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) {
-                      return ListOfStartups();
-                    },
-                  ),
-                );
-              },
-            ),
-            SizedBox(height: size.height * 0.03),
-          ],
+          ),
         ),
-      ),
+        Center(
+          child: Card(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Container(
+              height: 260,
+              width: 300,
+              padding: EdgeInsets.all(16),
+              child: Form(
+                  key: _formKey,
+                  child: SingleChildScrollView(
+                    child: Column(
+                      children: <Widget>[
+                        TextFormField(
+                          decoration: InputDecoration(labelText: 'Email'),
+                          keyboardType: TextInputType.emailAddress,
+                          validator: (value) {
+                            if (value.isEmpty) {
+                              return "Invalid Email";
+                            } else
+                              return null;
+                          },
+                          onSaved: (value) {
+                            _authData['email'] = value;
+                          },
+                        ),
+                        TextFormField(
+                          decoration: InputDecoration(labelText: 'Password'),
+                          obscureText: true,
+                          keyboardType: TextInputType.emailAddress,
+                          validator: (value) {
+                            if (value.isEmpty) {
+                              return "Password not valid";
+                            } else
+                              return null;
+                          },
+                          onSaved: (value) {
+                            _authData['password'] = value;
+                          },
+                        ),
+                        SizedBox(
+                          height: 30,
+                        ),
+                        RaisedButton(
+                          child: Text('Submit'),
+                          onPressed: () {
+                            submit();
+                          },
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          color: Colors.blueAccent,
+                          textColor: Colors.white,
+                        ),
+                      ],
+                    ),
+                  )),
+            ),
+          ),
+        )
+      ],
     );
   }
 }
