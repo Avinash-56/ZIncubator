2d7d1ba375ba34ac8436fed6dd78cc53a91e3231
unknown <avinashupadhyay56@gmail.com>
2020-12-23 15:40:39 +0530

    Signup bug fixed

--- lib/Screens/Signup/components/body.dart
+++ lib/Screens/Signup/components/body.dart
@@ -73,14 +73,14 @@ class _BodyState extends State<Body> {
     _formKey.currentState.save();
 
     try {
-      await Provider.of<AuthenticationService>(context, listen: false).signIn(
+      await Provider.of<AuthenticationService>(context, listen: false).signUp(
         _authData['email'],
         _authData['password'],
       );
       final SharedPreferences sharedPreferences =
           await SharedPreferences.getInstance();
       sharedPreferences.setString('email', _authData['email']);
-      Navigator.of(context).pushReplacementNamed(UserStartup.routeName);
+      Navigator.of(context).pushReplacementNamed(StartupForm.routeName);
     } catch (e) {
       var errorMessage = 'Authentication Failed. Try Again';
       _showErrorDialoge(errorMessage);
@@ -91,87 +91,6 @@ class _BodyState extends State<Body> {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
 
-    // return Stack(
-    //   children: <Widget>[
-    //     Container(
-    //       decoration: BoxDecoration(
-    //         gradient: LinearGradient(
-    //           colors: [
-    //             Colors.lightGreenAccent,
-    //             Colors.blue,
-    //           ],
-    //         ),
-    //       ),
-    //     ),
-    //     Center(
-    //       child: Card(
-    //         shape: RoundedRectangleBorder(
-    //           borderRadius: BorderRadius.circular(10),
-    //         ),
-    //         child: Container(
-    //           height: 260,
-    //           width: 300,
-    //           padding: EdgeInsets.all(16),
-    //           child: Form(
-    //               key: _formKey,
-    //               child: SingleChildScrollView(
-    //                 child: Column(
-    //                   children: <Widget>[
-    //                     TextFormField(
-    //                       decoration: InputDecoration(labelText: 'Email'),
-    //                       keyboardType: TextInputType.emailAddress,
-    //                       validator: (value) {
-    //                         if (value.isEmpty) {
-    //                           return "Invalid Email";
-    //                         } else
-    //                           return null;
-    //                       },
-    //                       onSaved: (value) {
-    //                         _authData['email'] = value;
-    //                       },
-    //                     ),
-    //                     TextFormField(
-    //                       decoration: InputDecoration(labelText: 'Password'),
-    //                       obscureText: true,
-    //                       keyboardType: TextInputType.emailAddress,
-    //                       validator: (value) {
-    //                         if (value.isEmpty) {
-    //                           return "Password not valid";
-    //                         } else
-    //                           return null;
-    //                       },
-    //                       onSaved: (value) {
-    //                         _authData['password'] = value;
-    //                       },
-    //                     ),
-    //                     SizedBox(
-    //                       height: 30,
-    //                     ),
-    //                     RaisedButton(
-    //                       child: Text('Submit'),
-    //                       onPressed: () {
-    //                         submit();
-    //                       },
-    //                       shape: RoundedRectangleBorder(
-    //                         borderRadius: BorderRadius.circular(10),
-    //                       ),
-    //                       color: Colors.blueAccent,
-    //                       textColor: Colors.white,
-    //                     ),
-    //                     AlreadyHaveAnAccountCheck(
-    //                       press: () {
-    //                         Navigator.of(context)
-    //                             .pushReplacementNamed(SignUpScreen.routeName);
-    //                       },
-    //                     ),
-    //                   ],
-    //                 ),
-    //               )),
-    //         ),
-    //       ),
-    //     )
-    //   ],
-    // );
     return Background(
         child: Form(
       key: _formKey,
@@ -212,14 +131,6 @@ class _BodyState extends State<Body> {
                   return null;
               },
             ),
-            // RoundedPasswordField(
-            //   validator: (value) {
-            //     if (value != _authData['password']) {
-            //       return "Password do not match";
-            //     } else
-            //       return null;
-            //   },
-            // ),
             RoundedButton(
               text: "SIGNUP",
               press: () {
